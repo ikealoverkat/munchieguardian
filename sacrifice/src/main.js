7a73d65b00b487582082a094c25a64539036bf0c
kat <katwang7749@gmail.com>
2025-09-27 18:30:26 -0400

    added bullets and opps but the opps kinda dont work

--- sacrifice/src/main.js
+++ sacrifice/src/main.js
@@ -11,20 +11,30 @@ kaplay({
 
 loadRoot("./"); // A good idea for Itch.io publishing later
 loadSprite("wizard", "sprites/wizard.png");
+loadSprite("bullet", "sprites/bullet.png");
+loadSprite("opp", "sprites/opp.png");
+loadSprite("opp2", "sprites/opp2.png");
+loadSprite("opp3", "sprites/opp3.png");
 
 scene("wave_1", () => {
    let wizardX = 120;
    let wizardY = 80;
 
     const wizard = add([
-        pos(120, 80), {
+        pos(wizardX, wizardY), {
             speed: 150
         },
         sprite("wizard"),
         scale(0.04),
         area(),
+        anchor("center"),
+        rotate(0),
     ]);
-
+    
+    wizard.onUpdate(() => {
+        wizard.rotateTo(mousePos().angle(wizard.pos));
+     });
+    
   wizard.onKeyDown((key) => {
     if (key === "w") {
         wizard.move(0, -wizard.speed)
@@ -39,6 +49,46 @@ scene("wave_1", () => {
         wizard.move(wizard.speed, 0)
     }    
   })
+
+    onClick(() => {
+        add([
+            sprite("bullet"),
+            pos(wizard.pos),
+            scale(0.3),
+            area(),
+            anchor(vec2(-4, -4)),
+            offscreen({
+                destroy: true,
+            }),
+            rotate(wizard.angle),
+            move(mousePos().sub(wizard.pos).unit(), 400),
+        ]);
+    });
+
+    const opps = [];
+
+    function addOpp() {
+        const options = ["opp","opp2","opp3"];
+        const index = Math.floor(rand(0, options.length));
+        const randomSprite = options[index];
+        
+        const newOpp = add([
+                sprite(randomSprite),
+                pos(rand(vec2(width(),height()))),
+                anchor("bot"),
+                scale(0.03),            
+                area(),
+                z(1),
+                "opp",
+                {speed: rand(25000, 30000)},
+            ]);
+
+            opps.push(newOpp);
+    }
+
+    loop(1.5, () => {
+        addOpp();
+    });
 
 });
 
